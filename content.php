9f27f4d3566fa1e43c1b958c679d6acc9a999b66
Lluis Casamajor <lluischess@gmail.com>
2022-01-31 23:20:03 +0100

    css central

--- content.php
+++ content.php
@@ -1,23 +1,24 @@
 <div id="central">
+    <h1>Productos destacados</h1>
     <div class="product">
         <img src="assets/img/logo.PNG" alt="Producto">
         <h2>Marines Espaciales</h2>
         <p>Miniaturas de Warhammer</p>
         <p>30 euros</p>
-        <a href="">Comprar</a>
+        <a href="" class="button">Comprar</a>
     </div>
     <div class="product">
         <img src="assets/img/logo.PNG" alt="Producto">
         <h2>Marines Espaciales</h2>
         <p>Miniaturas de Warhammer</p>
         <p>30 euros</p>
-        <a href="">Comprar</a>
+        <a href="" class="button">Comprar</a>
     </div>
     <div class="product">
         <img src="assets/img/logo.PNG" alt="Producto">
         <h2>Marines Espaciales</h2>
         <p>Miniaturas de Warhammer</p>
         <p>30 euros</p>
-        <a href="">Comprar</a>
+        <a href="" class="button">Comprar</a>
     </div>
 </div>
