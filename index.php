db18671723b779673ab4172aeb800aa95daf5a77
Lluis Casamajor <lluischess@gmail.com>
2022-02-06 23:02:18 +0100

    Creando MVC

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
+    <?php 
+    
+    include_once("autoload_lluis.php");
+    
+    if(isset($_GET['controller'])){
+        $nombre_controlador = $_GET['controller'].'Controller';
+    } else {
+        echo "La pagina no existe";
+        exit();
+    }
+
+    if(class_exists($nombre_controlador)){
+        $controlador = new $nombre_controlador();
+
+        if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+            $action = $_GET['action'];
+            $controlador->$action();
+        } else {
+            echo "La pagina no existe";
+        }
+
+    } else {
+        echo "La pagina no existe";
+    }
+    
+    
+    
+    ?>
     <!-- HEADER -->
+    
     <?php include_once("header.php"); ?>
 
     <!-- NAV -->
