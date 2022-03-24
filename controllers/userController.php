13148ea3edf5699cda9f57ea213a2539c6ec03f4
Lluis Casamajor <lluischess@gmail.com>
2022-03-24 21:12:21 +0100

    Cerrar session funciona bien

--- controllers/userController.php
+++ controllers/userController.php
@@ -1,58 +1,75 @@
 <?php
 
-    include_once "modelo/usuario.php";
+include_once "modelo/usuario.php";
 
-class userController{
+class userController
+{
 
-    public function index(){
+    public function index()
+    {
         echo "Controlador user index";
     }
 
-    public function registro(){
+    public function registro()
+    {
         require_once "views/user/registro.php";
     }
 
-    public function save(){
-        if(isset($_POST)){
+    public function save()
+    {
+        if (isset($_POST)) {
             $usuario = new Usuario();
             $usuario->setNombre($_POST['nombre']);
             $usuario->setApellido($_POST['apellidos']);
             $usuario->setEmail($_POST['email']);
             $usuario->setPassword($_POST['pass']);
-            
-            if ($usuario->save()){
+
+            if ($usuario->save()) {
                 $_SESSION['register'] = "complete";
                 echo "Usuario registrado correctamente";
-            }else{
+            } else {
                 echo "Error al guardar el usuario";
                 $_SESSION['register'] = "failed";
             }
         } else {
             $_SESSION['register'] = "failed";
         }
-        header("Location:".domain.'userController/registro');
+        header("Location:" . domain . 'userController/registro');
     }
 
-    public function login(){
+    public function login()
+    {
         if (isset($_POST)) {
             // Itentificar usuario
             $usuario = new Usuario();
             $usuario->setEmail($_POST['email']);
             $usuario->setPassword($_POST['password']);
-            
+
             $login = $usuario->login();
             // Crear sessión de login
-            if ($login && is_object($login)){
+            if ($login && is_object($login)) {
                 $_SESSION['user_login'] = $login;
-                
-                if ($login->rol == 'admin'){
+
+                if ($login->rol == 'admin') {
                     $_SESSION['admin_login'] = true;
                 }
-            }else{
+            } else {
                 $_SESSION['error_login'] = "Error al identificarte";
             }
         }
 
-        header("Location".domain);
+        header("Location:" . domain);
+    }
+
+    public function logout()
+    {
+        if (isset($_SESSION['admin_login'])) {
+            unset($_SESSION['admin_login']);
+        }
+        if (isset($_SESSION['user_login'])) {
+            unset($_SESSION['user_login']);
+        }
+
+        header("Location:". domain);
     }
 }
--- views/layout/lateral.php
+++ views/layout/lateral.php
@@ -13,7 +13,9 @@
             <h3><?= $_SESSION['user_login']->name_user. " " .$_SESSION['user_login']->lastname ?></h3>
         <?php endif; ?>
         <ul>
-            <li><a href="<?=domain?>userController/logout">Cerrar sesión</a></li>
+            <?php if (isset($_SESSION['user_login']) || isset($_SESSION['admin_login'])) { ?>
+                <li><a href="<?=domain?>userController/logout">Cerrar sesión</a></li>
+            <?php } ?>
             <li><a href="">Mis pedidos</a></li>
             <li><a href="">Gestionar categorias</a></li>
         </ul>
